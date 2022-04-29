0e27de5854902999943bafa433b0752adc9e0aee
MrSetOne <lara.sanchez.michael@gmail.com>
2022-04-29 17:47:49 +0200

    Psw verification

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,13 @@
 // Asignacion de variables
 const form = document.getElementById("form");
 const btn = document.getElementById("btn");
+let hotDB = []
 
 // Libreria de verificaciones
 const regex = {
     firstname: /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,
     email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 }
-
-// Libreria de funciones
-
-
-// 
-
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -26,10 +21,12 @@ form.addEventListener("submit", (e) => {
 
     // Inicio de las validaciones
     if ((user.name == "") || (user.mail == "") || (user.gender == "") || (user.psw1 == "") || (user.psw2 == "")) {
-        // Aquí falta que se genere un alert indicando que se rellenen todos los campos
+        //* Aquí falta que se genere un alert indicando que se rellenen todos los campos
     };
-    if (regex.firstname.test(user.name) && regex.email.test(user.mail)) { //Aqui falta la verificacion de genero
-        // Si acepta el patrón se genera el codigo de dentro
+    if (user.psw1 != user.psw2) {
+        //* Aqui hay que poner un alert indicando que las contraseñas no coinciden
+    }
+    if (regex.firstname.test(user.name) && regex.email.test(user.mail)) {
+        //* Si acepta el patrón se genera el codigo de dentro
     };
-    console.log(user.gender)
 })
--- test.js
+++
@@ -1,5 +0,0 @@
-const tester = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
-const toTest = "Julian Rodriguez"
-
-const resultado = tester.test(toTest)
-console.log(resultado)
